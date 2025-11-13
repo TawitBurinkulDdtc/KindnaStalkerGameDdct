a0e0d36b8deeba82037b439351239fe1f5c10a83
TawitBurinkulDdtc <141985513+TawitBurinkulDdtc@users.noreply.github.com>
2025-11-13 21:15:38 +0700

    Make game easier and make player float

--- Assets/Codes/PlayerGetKillByAnomally.cs
+++ Assets/Codes/PlayerGetKillByAnomally.cs
@@ -5,10 +5,11 @@ public class PlayerGetKillByAnomally : MonoBehaviour
 
     public string tagName;
     [SerializeField] GameObject deadUi;
-
+    [SerializeField] PlayerMovement pm;
     void OnTriggerEnter(Collider other){
        if(other.gameObject.tag == tagName){
             deadUi.SetActive(true);
+            pm.gravity = 9.8f;
         }
     }
 }
